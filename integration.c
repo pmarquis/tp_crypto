b8f6c6d0ec78d09fd98fddaa40b63e5130a673a2
Mathéo Morin <91942291+Matheoleharicot@users.noreply.github.com>
2026-01-14 16:21:08 +0100

    Add SubBytes function implementation and declaration

--- integration.c
+++ integration.c
@@ -3,6 +3,19 @@
 typedef unsigned char u8;
 typedef unsigned int u32;
 
+void SubBytes(u8 in_state[4][4], u8 out_state[4][4])
+{
+    u8 r, c;
+    for (r = 0; r < 4; r++) {
+        for (c = 0; c < 4; c++) {
+            u8 a = in_state[r][c];
+            u8 x = (u8)(a >> 4);
+            u8 y = (u8)(a & 0x0F);
+            out_state[r][c] = s_box[x][y];
+        }
+    }
+}
+
 void Cipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]) {
 
 }
--- integration.h
+++ integration.h
@@ -1,2 +1,3 @@
 void Cipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]);
 void InvCipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]);
+void SubBytes(u8 in_state[4][4], u8 out_state[4][4]);
