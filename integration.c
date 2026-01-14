b5255a10cd47eddc009da50c6ab2a62f46a5898f
Pierre M. <pier.marquis@gmail.com>
2026-01-14 16:09:06 +0100

    First Commit

---
+++ integration.c
@@ -0,0 +1,11 @@
+#include "integration.h"
+#include <stdio.h>
+typedef unsigned char u8;
+typedef unsigned int u32;
+
+void Cipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]) {
+
+}
+void InvCipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]) {
+
+}
---
+++ integration.h
@@ -0,0 +1,2 @@
+void Cipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]);
+void InvCipher(u8 in[4*Nb], u8 out[4*Nb], u32 sub_key[Nb*(Nr+1)]);
